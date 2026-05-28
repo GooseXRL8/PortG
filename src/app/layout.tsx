2d3f5e04aafc3f838dccb8ccfe0f6e1dacb87fd6
GooseXRL8 <elielvanda10@gmail.com>
2026-05-27 22:33:34 -0300

    logo 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="icon" href="/pg.png" sizes="1254x1254" />
+        <link rel="icon" href="/pg.ico" sizes="48x48" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500;700&display=swap"
